884f64097ec1890b68ac6a60ae44b4e8f0e18f12
tismajain <159927171+tismajain@users.noreply.github.com>
2026-06-12 17:09:39 +0530

    Add: binary-tree-diameter - submission-1

---
+++ Data Structures & Algorithms/binary-tree-diameter/submission-1.java
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    int max=0;
+    int length(TreeNode root)
+    {
+        if(root==null) return 0;
+        int left=length(root.left);
+        int right=length(root.right);
+        max=Math.max(max,left+right);
+        return 1+Math.max(left,right);
+    }
+    public int diameterOfBinaryTree(TreeNode root) {
+        if(root==null) return 0;
+        length(root);
+        return max;
+        
+
+    }
+}
